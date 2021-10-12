45ce4ead5bdca79b935ed2c2987f5e053876cb6b
pupil <pupil@ivkhk.ee>
2021-10-12 11:16:19 +0300

    first commit

---
+++ src/YrovClass/Yrovnenija_Class.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package YrovClass;
+
+/**
+ *
+ * @author pupil
+ */
+public class Yrovnenija_Class {
+     public double Yrovnenie(float A, double B, double C,double x){
+         
+ return A*(x*x)+B*x+C;
+}
+}
